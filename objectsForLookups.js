ceb016d26eaac49adb50aafc16f208488e098c00
Benjamin Benkert <ben@peakperformancepc.com>
2018-06-06 16:02:49 -0400

    Start Challenge using objects for lookups

---
+++ objectsForLookups.js
@@ -0,0 +1,33 @@
+/*Convert the switch statement into an object called lookup. 
+Use it to look up val and assign the associated string to the result variable.
+*/
+function phoneticLookup(val) {
+    var result = "";
+  
+    // Only change code below this line
+    switch(val) {
+      case "alpha": 
+        result = "Adams";
+        break;
+      case "bravo": 
+        result = "Boston";
+        break;
+      case "charlie": 
+        result = "Chicago";
+        break;
+      case "delta": 
+        result = "Denver";
+        break;
+      case "echo": 
+        result = "Easy";
+        break;
+      case "foxtrot": 
+        result = "Frank";
+    }
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  // Change this value to test
+  phoneticLookup("charlie");
